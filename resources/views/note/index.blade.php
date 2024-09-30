077977aeda461059041f6bb2d2451fdf0f224b0c
ajdude9 <adamtheteeth@hotmail.com>
2024-09-30 17:47:37 +0100

    CSS style addition

--- resources/views/note/index.blade.php
+++ resources/views/note/index.blade.php
@@ -1,18 +1,21 @@
 <x-layout>
     <h1>Index</h1>
-    <a href="#" class="new-note-btn">
+    <a href="{{ route('note.create' }}" class="new-note-btn">
         New Note
     </a>
     <div class="notes">
-        <div class="note">
-            <div class="note-body">
-                Lorem, ipsum dolor sit amet consectetur adipiscing elit.
+        @foreach ($notes as $note)
+            <div class="note">
+                <div class="note-body">
+                    {{ Str::words($note->note, 30) }}
+                </div>
+                <div class="note-buttons">
+                    <a href="{{ route('note.show', $note }}" class="note-edit-button">View</a>
+                    <a href="{{ route('note.edit', $note }}" class="note-edit-button">Edit</a>
+                    <button class="note-delete-button">Delete</button>
+                </div>
             </div>
-            <div class="note-buttons">
-                <a href="#" class="note-edit-button">View</a>
-                <a href="#" class="note-edit-button">Edit</a>
-                <button class="note-delete-button">Delete</button>
-            </div>
-        </div>
+        @endforeach
+        
     </div>
 </x-layout>
